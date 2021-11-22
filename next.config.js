7949d7f524e3430e4e05627ed85aa1c49412eb0a
claudiomonzoni <claudiomonzoni@hotmail.com>
2021-11-22 11:12:47 -0600

    se queda en el building

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  exportPathMap() {
-		return {
-			'/': { page: '/' },
-		}
-	},
+  // exportPathMap() {
+	// 	return {
+	// 		'/': { page: '/' },
+	// 	}
+	// },
   // https://github.com/vercel/next.js/issues/21079
   // Remove the workaround the issue is fixed
   reactStrictMode: true,
